f5646d78a00cbf51abfea5dbf90e873875859486
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-11-07 22:19:54 +0100

    7-11-2022

--- src/test/java/pojos/BookingDatesPojo.java
+++ src/test/java/pojos/BookingDatesPojo.java
@@ -1,5 +1,9 @@
 package pojos;
 
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookingDatesPojo {
 
     //1) Tüm keyler için private variable'lar oluşturuyoruz.
